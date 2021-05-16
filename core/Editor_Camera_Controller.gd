d8defdc23b6e73164f19a1568c84349c0c7b2f4b
ujjwal <ujjwalkumar3067@gmail.com>
2021-05-15 20:35:32 -0400

    added process movement

--- core/Editor_Camera_Controller.gd
+++ core/Editor_Camera_Controller.gd
@@ -7,6 +7,7 @@ signal physics_object_selected(object)
 
 # class variables
 const NORMAL_COLLISION_LAYER  = -1
+const CONTROL_SPEED = 2
 const MOVE_SPEED = 2
 const SHIFT_SPEED = 20
 const MOUSE_SENSITIVITY = 0.05
@@ -37,5 +38,22 @@ func process_movement(delta):
 	var movement_vector = Vector3(0,0,0)
 	var movement_speed = MOVE_SPEED  # scalar
 
-	# freelook camera input
-	if (Input.is_action_just_pressed("editor_move_backward")== true):
+	# freelook camera input gg
+
+	# back and front movement
+	if (Input.is_action_just_pressed("editor_move_forward")== true):
+		movement_vector.z = 1
+	elif (Input.is_action_just_pressed("editor_move_backward") == true):
+		movement_vector.z = -1
+	# left and right movement
+	if (Input.is_action_just_pressed("editor_move_right") == true):
+		movement_vector.x = 1
+	elif(Input.is_action_just_pressed("editor_move_left") == true):
+		movement_vector.x = -1
+	if(Input.is_key_pressed(KEY_SHIFT)):
+		movement_speed = SHIFT_SPEED
+	elif(Input.is_key_pressed(KEY_CONTROL)):
+		movement_speed = CONTROL_SPEED
+
+	global_transform.origin += -view_camera.global_transform.basis.z * movement_vector.z *movement_speed*delta;
+	global_transform.origin += view_camera.global_transform.basis.x * movement_vector.x *movement_speed*delta;
