e20a0bf96436f01c18d751ded35c0cfed73b6e35
ujjwal <ujjwalkumar3067@gmail.com>
2021-05-15 21:11:42 -0400

    done with editor script

--- core/Editor_Camera_Controller.gd
+++ core/Editor_Camera_Controller.gd
@@ -12,7 +12,7 @@ const MOVE_SPEED = 2
 const SHIFT_SPEED = 20
 const MOUSE_SENSITIVITY = 0.05
 var view_camera = null
-const CAMER_MAX_ROTATION_ANGLE = 70
+const CAMERA_MAX_ROTATION_ANGLE = 70
 var send_raycast = false;
 
 func _ready():
@@ -57,3 +57,29 @@ func process_movement(delta):
 
 	global_transform.origin += -view_camera.global_transform.basis.z * movement_vector.z *movement_speed*delta;
 	global_transform.origin += view_camera.global_transform.basis.x * movement_vector.x *movement_speed*delta;
+
+func _unhandled_input(event):
+	if(editor_controller.is_in_freelook_mode == true):
+		if(event is InputEventMouseMotion):
+			var camera_rotation = view_camera.rotation_degrees
+			# NOTE (ujjwal) clamp the camera rotation to 70 degrees ( change it later if needed )
+			camera_rotation.x = clamp(camera_rotation.x + (-event.relative.y*MOUSE_SENSITIVITY), -CAMERA_MAX_ROTATION_ANGLE, CAMERA_MAX_ROTATION_ANGLE)
+			rotation_degrees.y += event.relative.x * MOUSE_SENSITIVITY
+			view_camera.rotation_degrees = camera_rotation
+	else:
+		if(event is InputEventMouseMotion):
+			if (event.button_index == BUTTON_LEFT and event.pressed == true)
+			if(editor_controller.editor_mode == "SELECT"):
+				send_raycast = true;
+
+func _physics_process(delta):
+	if (send_raycast == true):
+		send_raycast = false
+		var selected_node = null
+		var space_state = get_world().direct_space_state
+		var raycast_from = view_camera.project_ray_origin(get_tree().root.get_mouse_position())
+		var raycast_to = raycast_from + view_camera.project_ray_normal(get_tree().root.get_mouse_position())*100
+		var result = space_state.intersect_ray(raycast_from , raycast_to , [self], NORMAL_COLLISION_LAYER)
+		if(result.size()> 0 ):
+			selected_node = result.collider
+		emit_signal("physics_object_selected", selected_node)  #pass selected node as argument
